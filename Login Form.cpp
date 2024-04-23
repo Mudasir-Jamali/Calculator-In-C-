5c68b4d00467ee64103714283cfbc10d4c94619b
Mudasir-Jamali <167112027+Mudasir-Jamali@users.noreply.github.com>
2024-04-23 09:28:25 +0500

    Login-Form
    Simple Login Form in C++

--- Login Form.cpp
+++ Login Form.cpp
@@ -1,150 +1,64 @@
 #include <iostream>
-#include <fstream>
-#include <string.h>
+#include <string>
+#include <map>
+
 using namespace std;
 
-void login();
-void registration();
-void forgot();
+// Define a struct to represent user data
+struct UserData
+{
+    string username;
+    string password;
+};
 
 int main()
 {
-    int c;
-    cout << "\t\t\t_________________________________________\n\n\n";
-    cout << "\t\t\t          welcome to the login page      \n\n\n";
-    cout << "\t\t\t_________        MENU        ____________\n\n\n";
-    cout << "                                                 \n\n\n";
-    cout << "\t|  Press 1 to LOGIN                     |" << endl;
-    cout << "\t|  Press 2 to REGISTER                  |" << endl;
-    cout << "\t|  Press 3 if you forgot your PASSWORD  |" << endl;
-    cout << "\n\t\t\t Please enter your choice : ";
-    cin >> c;
-    cout << endl;
+    map<string, UserData> users;
 
-    switch (c)
+    // Add default user
+    users["mudasir"] = {"mudasir", "jamali"};
+
+    string username, password;
+    char choice;
+
+    do
     {
-    case 1:
-        login();
-        break;
+        // Prompt the user to enter username
+        cout << "Enter username: ";
+        cin >> username;
 
-    case 2:
-        registration();
-        break;
+        // Prompt the user to enter password
+        cout << "Enter password: ";
+        cin >> password;
 
-    case 3:
-        forgot();
-        break;
-
-    case 4:
-        cout << "\t\t\t Thankyou! \n\n\n";
-        break;
-
-    default:
-        system("cls");
-        cout << "\t\t\t Pleae select from the option given above \n"
-             << endl;
-        main();
-    }
-}
-
-void login()
-{
-    int count;
-    string userId, password, id, pass;
-    system("cls");
-    cout << "\t\t\t Please enter the username and password : " << endl;
-    cout << "\t\t\t USERNAME ";
-    cin >> userId;
-    cout << "\t\t\t PASSWORD ";
-    cin >> password;
-
-    ifstream input("record.txt");
-
-    while (input >> id >> pass)
-    {
-        if (id == userId && pass == password)
+        // Check if the entered username and password are correct
+        if (users.count(username) > 0 && users[username].password == password)
         {
-            count = 1;
-            system("cls");
-        }
-    }
-    input.close();
-
-    if (count == 1)
-    {
-        cout << userId << "\n Your LOGIN is successfull /n Thanks for logging in ! \n";
-        main();
-    }
-    else
-    {
-        cout << "\n LOGIN ERROR \n Please check your username and password\n";
-        main();
-    }
-}
-
-void registration()
-{
-    string ruserId, rpassword, rid, rpass;
-    system("cls");
-    cout << "\t\t\t Enter the username : ";
-    cin >> ruserId;
-    cout << "\t\t\t Enter the password : ";
-    cin >> rpassword;
-
-    ofstream f1("records.txt", ios::app);
-    f1 << ruserId << " " << rpassword << endl;
-    system("cls");
-    cout << "\n\t\t\t Registrationis succesfull! \n";
-    main();
-}
-
-void forgot()
-{
-    int option;
-    system("cls");
-    cout << "\t\t\t You forgot the password? No Worries \n";
-    cout << "Press 1 to search your id by username " << endl;
-    cout << "press 2 to go to the main menu " << endl;
-    cout << "\t\t\t Enter your choice :";
-    cin >> option;
-    switch (option)
-    {
-    case 1:
-    {
-        int count = 0;
-        string suserId, sId, spass;
-        cout << "\n\t\t\t Enter the username which you remembered :";
-        cin >> suserId;
-
-        ifstream f2("records.txt");
-        while (f2 >> sId >> spass)
-        {
-            if (sId == suserId)
-            {
-                count = 1;
-            }
-        }
-        f2.close();
-        if (count == 1)
-        {
-            cout << "\n\n Your account is found \n";
-            cout << "\n\n Your password is :" << spass;
-            main();
+            cout << "Login successful! Welcome, " << username << "." << endl;
         }
         else
         {
-            cout << "\n\t Sorry! your account is not found! \n";
-            main();
+            cout << "Incorrect username or password. Access denied." << endl;
+            cout << "Do you want to register? (y/n): ";
+            cin >> choice;
+
+            if (choice == 'y')
+            {
+                // Register a new user
+                cout << "Enter new username: ";
+                cin >> username;
+                cout << "Enter new password: ";
+                cin >> password;
+
+                // Add the new user to the map
+                users[username] = {username, password};
+                cout << "Registration successful! You can now login with your new account." << endl;
+            }
         }
 
-        break;
-    }
-    case 2:
-    {
-        main();
-    }
-    default:
-        cout << "\t\t\t Wrong choice ! Please try again " << endl;
-        forgot();
-    }
+        cout << "Do you want to login again? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y');
+
+    return 0;
 }
